7a187c15a5cee896393bca309b0a348986cdf6bf
Charles Grandison <therealakeem03@gmail.com>
2022-01-11 06:14:38 +0100

    More UI fixes

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,9 @@
-const svgBox = document.querySelector('.second__column')
+const svgBox = document.querySelector(".second__column");
+const first__column = document.querySelector(".first__column");
+const third__column = document.querySelector(".third__column");
 
-svgBox.addEventListener('click', (e) => {
-    svgBox.classList.toggle('rotate')
-})
+svgBox.addEventListener("click", (e) => {
+  svgBox.classList.toggle("rotate");
+  first__column.classList.toggle('translate__first')
+  third__column.classList.toggle('translate__third')
+});
